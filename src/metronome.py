ae9075852d3d91eef0066b350ecd2bef459ef460
johnnyhomeless <giovolo@gmail.com>
2025-02-07 19:12:02 +0100

    Enhanced Metronome class structure with timing and playback

--- src/metronome.py
+++ src/metronome.py
@@ -11,13 +11,26 @@ from constants import (SOUND_FILE,
 
 
 class Metronome:
-    def __init__(self, bpm=None):
+    def __init__(self, bpm):
+        self.is_running = False
+        self.bpm = bpm
+        self.interval = self.calculate_interval()
+
         try:
             pygame.mixer.init()
         except pygame.error:
             print(CURRENT_LANG["PYMIXER_ERROR"])
             return
     
+    def calculate_interval(self):
+        return 60 / self.bpm
+    
+    def play_beats(self):
+        while self.is_running:
+            if self.sound:
+                self.sound.play()
+                time.sleep(self.interval)
+
     def load_sound(self):
         if check_wav_file():
             self.sound = pygame.mixer.Sound(SOUND_FILE)
@@ -29,5 +42,4 @@ class Metronome:
 
 def check_wav_file():
     path = Path(SOUND_FILE)
-    return path.is_file()
-        
+    return path.is_file()  
